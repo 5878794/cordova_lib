37cfc2d03c56023ce34f379f870d5cdd4545f978
beens <5878794@qq.com>
2016-07-27 11:01:43 +0800

    add  page ready  api

--- js/lib/capp.js
+++ js/lib/capp.js
@@ -15,3 +15,44 @@ if(!window.CAPP){CAPP = {};}
 CAPP.Exit = function(){
     navigator.app.exitApp();
 };
+
+
+//页面准备好
+CAPP.ready = (function(){
+    var a = 0,
+        isReady = false,
+        cache = [];
+
+    var init = function(){
+        a++;
+        if(a == 2){
+            isReady = true;
+            run();
+        }
+    };
+
+    document.addEventListener("deviceready",function(){
+        init();
+    },false);
+    $(document).ready(function(){
+        init();
+    });
+
+
+
+    var run = function(){
+        for(var i = 0,l=cache.length;i<l;i++){
+            cache[i]();
+        }
+    };
+
+
+    return function(fn){
+        if(isReady){
+            fn();
+        }else{
+            cache.push(fn);
+        }
+    };
+
+})();
